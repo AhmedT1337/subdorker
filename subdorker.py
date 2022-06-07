2c998e378225754c3d959fb93b55cb84d3054264
Ahmed Tareq <63518121+AhmedT1337@users.noreply.github.com>
2022-06-07 13:27:22 -0400

    Add files via upload

---
+++ subdorker.py
@@ -0,0 +1,61 @@
+from sys import argv
+from googlesearch import search
+from art import tprint
+
+tprint("SUB-DORK-ER")
+print("			Author : @AHMEDT1337")
+
+
+
+def usage() :
+    print("""
+####################################################
+
+Usage: 
+	- If you want to dork for  subdomains : 
+
+		python3 subdorker.py -d domain
+
+	- If you want to dork for sub-subdomains :
+
+		python3 subdorker.py -d *.domain
+
+	- If you want this usage menu :
+
+		python3 subdorker.py --help
+
+####################################################
+	    """)
+
+
+
+def dorking(domain) :
+
+	query = f"site:*.{domain}"
+
+	sh = search(query)
+
+
+	file = open(f"{domain}.txt", "a")
+
+
+	for item in sh :
+
+		file.writelines(domain+"\n")
+
+	file.close()
+
+	print(f"Results are put in a file named {query}.txt")
+
+
+
+if len(argv) < 3 or argv[1].lower() == '--help' or argv[1].lower() == '-help' :
+
+    usage()
+
+
+elif argv[1].lower() == '-d' :
+
+    domain = str(argv[2])
+    
+    dorking(domain)
